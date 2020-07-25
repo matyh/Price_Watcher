9958f17ac95245ff0caf403cbe234ef3a24a8242
Matyho <martin.hora11gmail.com>
2020-07-25 10:00:36 +0200

    pandas

--- main.py
+++ main.py
@@ -1,14 +1,35 @@
-from bs4 import BeautifulSoup
+import lxml.html
 import requests
-from decimal import Decimal
-import re
+import csv
+import bs4
+from price_parser import Price
+import pandas as pd
 
+eur_czk_rate = 26.25
 
-URL = 'https://www.canyon.com/cs-cz/mountain-bikes/trail-bikes/neuron/neuron-cf-8.0/2471.html'
-page = requests.get(URL)
+# load data into df
+data_df = pd.read_csv('config.csv', delimiter=',', header=0)
 
-soup = BeautifulSoup(page.content, features='html.parser')
-price_raw = soup.find('div', class_="productDescription__priceSale")
-price = price_raw.text.strip()
-price = re.sub(r'[^\d,]', '', price).replace(',', '.')
-print(float(price))
+url = data_df['url']
+price_selector = data_df['price_selector']
+name_selector = data_df['name']
+
+for u, p, n in zip(url, price_selector, name_selector):
+    page = requests.get(u)
+    soup = bs4.BeautifulSoup(page.content, 'html.parser')
+    name = n
+
+    price = soup.select(p)
+    if len(price) > 1 and not all([p == price[0] for p in price]):
+        # following is for debugging
+        print(u)
+        for i in price:
+            print(i)
+        #
+        raise ValueError("Multiple results")
+    else:
+        result = price[0].get('content')
+        if result is None:
+            result = price[0].get('data-price')
+
+    print(f'{name:<30} {result}')
